df6b2168baab238c38cca124e6c3348728df6f43
Leonardo Castilho <leonardo.castilho@viannasempre.com.br>
2022-09-13 15:14:53 -0300

    Navegando entre páginas
    Navegando da página principal para a página de detalhes quando se clica em um card.

--- Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
+++ Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
@@ -16,10 +16,11 @@ public static class MauiProgram
 			});
 
 		builder.Services.AddSingleton<MonkeyService>();
-
 		builder.Services.AddSingleton<MonkeysViewModel>();
+		builder.Services.AddTransient<MonkeyDetailsViewModel>();
 
 		builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddTransient<DetailsPage>();
 
 		return builder.Build();
 	}
